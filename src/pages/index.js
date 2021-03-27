2668a09a008f2910e62f25fa1df212f07389db1d
brianpompey <brianthescholar@gmail.com>
2021-03-26 23:45:33 -0400

    ok ive separated sections for each type to go next is to populate each section

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,34 @@ const IndexPage = () => (
     </div>
     <p>My name is Chef Brian Pompey and my first love, my first passion is true American BBQ. Among my many culinary dreams, I have a huge desire to help everyday aspiring "cue-masters" reach the peaks of their potential. One of the most important factors in good bbq is the tool that's being used. According to the legend, John Lewis of Lewis BBQ fame, the cooking vessel is the single most important factor. Now I agree to some extent. While it's easy for us experienced enthusiasts to think we have some king of extra special sauce that can't be duplicated, to be honest it really isn't rocket science. Similar to programming, there are so many ways to put together a great dish and the right amount of attention to detail, patience and of course the right vessel or tools will definitely make your job so much easier and successful. So my goal on this site is to address that factor. Helping the new bbq enthusiast choose the best tool of the trade for their goals.</p>
     
+    <hr />
+
+    <div className="smokers">
+      <div className="smokers-charcoal">
+        <p>charcoal</p>
+      </div>
+    <hr />
+      <div className="smokers-electric">
+        <p>electric</p>
+      </div>
+    <hr />
+      <div className="smokers-pellet">
+        <p>pellet</p>
+      </div>
+    <hr />
+      <div className="smokers-ceramic">
+        <p>ceramic</p>
+      </div>
+    <hr />
+      <div className="smokers-offset">
+        <p>offset</p>
+      </div>
+    <hr />
+      <div className="smokers-gravity">
+        <p>gravity</p>
+      </div>
+    <hr />
+    </div>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
