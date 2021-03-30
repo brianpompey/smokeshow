84304211bcbfd9df24b961a640e5ad8aed1407ba
brianpompey <brianthescholar@gmail.com>
2021-03-30 16:07:09 -0400

    hard time

---
+++ src/components/Paid.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './layout'
+import Layout from './layout';
+
+const Paid = () => (
+    <Layout>
+        <div className="BlogGroup">
+            
+            <div className="BlogInfo">
+                <h1>COMING SOON!!!</h1>
+            </div>
+            
+        </div>
+    </Layout>
+)
+
+export default Paid;
