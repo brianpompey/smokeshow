0b0ce319d616038c0d4ad8250295d7bb8fb0a672
brianpompey <brianthescholar@gmail.com>
2021-03-30 16:34:54 -0400

    ill try agian later:

--- src/pages/paid.js
+++ src/pages/paid.js
@@ -1,15 +1,16 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import Paid from "../components/Paid"
 
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const PaidPage= () => (
-    <div>
-        <SEO title="Paid" />
-        <Paid/>
+    <Layout>
+        <SEO title="Page two" />
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2</p>
         <Link to="/">Go back to the homepage</Link>
-    </div>
+    </Layout>
 )
 
 export default PaidPage;
