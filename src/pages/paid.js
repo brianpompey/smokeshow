1949d5d5dc94ed869929ee91f2af0788865f22bc
brianpompey <brianthescholar@gmail.com>
2021-03-30 20:52:18 -0400

    i think theres just an issue with safari and gatsby links

--- src/pages/paid.js
+++ src/pages/paid.js
@@ -10,7 +10,7 @@ const PaidPage= () => (
         <h1>Hi from the second page</h1>
         <p>Welcome to page 2</p>
         <Link to="/">Go back to the homepage</Link>
-    </Layout>
+    </Layout> 
 )
 
 export default PaidPage;
