387ed51c103c1e604196e270d88f096836930889
brianpompey <brianthescholar@gmail.com>
2021-04-01 14:49:10 -0400

    added all kettles

---
+++ src/pages/smokers/kettle/kettlepro.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import SmokerInfo from "../../../components/SmokerInfo"
+import Layout from "../../../components/layout"
+
+import SEO from "../../../components/seo"
+
+const WeberPro = () => (
+    <Layout>
+        <SEO title="Weber Pro" />
+        <SmokerInfo 
+            title="Weber Kettle Performer"
+            text="The best one"
+        />
+        <Link to="/">Go back to the homepage</Link>
+    </Layout> 
+)
+
+export default WeberPro;
