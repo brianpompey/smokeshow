2851e532580e8678af118da29a22cb189b6e404a
brianpompey <brianthescholar@gmail.com>
2021-04-10 13:10:55 -0400

    added snipcart plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+			resolve: 'gatsby-plugin-snipcart',
+			options: {
+				apiKey: 'MzQ3ODRiYzUtYWMyZi00OWRiLWE4MjAtZmIzOWYxNTMzNTY2NjM3NTMxNTIxOTQxNjg4Nzg2'
+			}
+		},
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
