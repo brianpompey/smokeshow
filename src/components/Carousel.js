2e6aa6cb072b48a16127985626b393fa22ed16ad
brianpompey <brianthescholar@gmail.com>
2021-05-02 19:12:39 -0400

    images and legends set

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,7 +10,6 @@ import car9 from "../images/carousel9.jpg"
 import car10 from "../images/carousel10.jpg"
 import car11 from "../images/carousel11.jpg"
 import car12 from "../images/carousel12.jpg"
-import car13 from "../images/carousel13.jpg"
 import car14 from "../images/carousel14.jpg"
 
 import { Carousel } from "react-responsive-carousel";
@@ -21,51 +20,47 @@ export default () => (
     <Carousel width="80%" centerMode useKeyboardArrows>
       <div>
         <img alt="" src={car1} />
-        <p className="legend">Legend 1</p>
+        <p className="legend">Day 1 on the Weber</p>
       </div>
       <div>
         <img alt="" src={car2} />
-        <p className="legend">Legend 2</p>
+        <p className="legend">First shot at some wings</p>
       </div>
       <div>
         <img alt="" src={car3} />
-        <p className="legend">Legend 3</p>
+        <p className="legend">First Weber racks of ribs!</p>
       </div>
       <div>
         <img alt="" src={car4} />
-        <p className="legend">Legend 4</p>
+        <p className="legend">Great cut on those ribs</p>
       </div>
       <div>
         <img alt="" src={car5} />
-        <p className="legend">Legend 5</p>
+        <p className="legend">Elotes, Potatoes and Chicken with White Sauce</p>
       </div>
       <div>
         <img alt="" src={car8} />
-        <p className="legend">Legend 8</p>
+        <p className="legend">Ribs on the Pit Barrel</p>
       </div>
       <div>
         <img alt="" src={car9} />
-        <p className="legend">Legend 9</p>
+        <p className="legend">Smoked Jerk Chicken </p>
       </div>
       <div>
         <img alt="" src={car10} />
-        <p className="legend">Legend 10</p>
+        <p className="legend">Beautiful Brisket</p>
       </div>
       <div>
         <img alt="" src={car11} />
-        <p className="legend">Legend 11</p>
+        <p className="legend">Brisket n Beef Rib</p>
       </div>
       <div>
         <img alt="" src={car12} />
-        <p className="legend">Legend 12</p>
-      </div>
-      <div>
-        <img alt="" src={car13} />
-        <p className="legend">Legend 13</p>
+        <p className="legend">Snow Day Ribs</p>
       </div>
       <div>
         <img alt="" src={car14} />
-        <p className="legend">Legend 14</p>
+        <p className="legend">Ribs, Chicken, and Corn</p>
       </div>
     </Carousel>
 );
