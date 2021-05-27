348c278ad722a5db247fddd2e104cc084d315a9c
brianpompey <brianthescholar@gmail.com>
2021-05-26 20:26:18 -0400

    using some of the stuff from portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,22 @@
 /**need a footer so the bottom of the page isnt ugly anymore */
 import React from 'react'
 import './Footer.css'
+
+const Footer = ({data, children}) => (
+    <div className="Footer">
+        <div className="FooterText">
+            <p>Thanks for visiting my page! Hopefully we can work together at some point!</p>
+        </div>
+        <div className="FooterLinks">
+            {data.allContentfulLink.edges.map(edge => (
+        <a href={edge.node.url}>{edge.node.title}</a>
+      ))}
+        </div>
+        <div className="Copywright">
+            {children}
+        </div>
+    </div>
+
+)
+
+export default Footer;
