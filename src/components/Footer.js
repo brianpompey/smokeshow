85e641e85a22ca8b1b8c519ad2508466cb5cbec5
brianpompey <brianthescholar@gmail.com>
2021-05-26 20:27:51 -0400

    i dont think ill use any links at the footer for now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,10 @@
 import React from 'react'
 import './Footer.css'
 
-const Footer = ({data, children}) => (
+const Footer = () => (
     <div className="Footer">
         <div className="FooterText">
             <p>Thanks for visiting my page! Hopefully we can work together at some point!</p>
-        </div>
-        <div className="FooterLinks">
-            {data.allContentfulLink.edges.map(edge => (
-        <a href={edge.node.url}>{edge.node.title}</a>
-      ))}
-        </div>
-        <div className="Copywright">
-            {children}
         </div>
     </div>
 
