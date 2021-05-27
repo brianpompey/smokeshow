b888b433aa18eecf53c37b1c8845a54e35a63ca3
brianpompey <brianthescholar@gmail.com>
2021-05-26 20:40:46 -0400

    chnaged text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 const Footer = ({ children }) => (
     <div className="Footer">
         <div className="FooterText">
-            <p>Thanks for visiting my page! Hopefully we can work together at some point!</p>
+            <p>Thanks for visiting my page! Hopefully you were able to find what you were looking for to make great bbq!</p>
         </div>
         <div className="Copywright">
             {children}
